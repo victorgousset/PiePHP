c4a8649a970f231d0361cb14effc52434a84873e
Gousset <victor.gousset@epitech.eu>
2020-03-31 22:27:30 +0200

    Core/Request

--- Core/Request.php
+++ Core/Request.php
@@ -3,6 +3,29 @@
 class Request
 {
 
+    public function __construct()
+    {
+        $this->checkPOST();
+        $this->checkGET();
+    }
 
+    public function checkPOST()
+    {
+        if(count($_POST) > 0)
+        {
+            $im = implode(' ', $_POST);
+            return htmlentities($im);
+        }
+        return null;
+    }
 
+    public function checkGET()
+    {
+        if(count($_GET) > 0)
+        {
+            $im = implode(' ', $_GET);
+            return htmlentities($im);
+        }
+        return null;
+    }
 }
